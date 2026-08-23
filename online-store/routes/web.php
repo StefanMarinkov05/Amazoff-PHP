95d3dba86f793d156214971bafa9d76237200abd
Aleksandar Stanchev <altryx15@gmail.com>
2026-08-23 21:04:08 +0300

    style: order imports in routes/web.php

--- online-store/routes/web.php
+++ online-store/routes/web.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Illuminate\Support\Facades\Route;
 use App\Livewire\Catalogue\ProductList;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/catalogue', ProductList::class);
