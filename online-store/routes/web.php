f9a1cca885458b017b970e22d9220937df0b0b9b
Aleksandar Stanchev <altryx15@gmail.com>
2026-08-29 16:16:19 +0300

    Contact form + About-us
    Added action logic for subscribing to a newsletter and checks whether the guest has unsubscribed and then subscribed again.

--- online-store/routes/web.php
+++ online-store/routes/web.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 use App\Livewire\Catalogue\ProductDetails;
 use App\Livewire\Catalogue\ProductList;
+use App\Livewire\Contact\ContactForm;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/catalogue', ProductList::class);
 Route::get('/products/{product:slug}', ProductDetails::class);
+Route::view('/about', 'pages.about')->name('about');
+Route::get('/contact', ContactForm::class)->name('contact');
